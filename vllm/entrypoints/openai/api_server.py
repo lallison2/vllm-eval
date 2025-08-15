fdd662bdf86710ca82c4199cefdbb2cfb013dc68
lallison2 <lallison@mit.edu>
2025-08-15 15:14:42 -0700

    r

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -584,6 +584,9 @@ async def create_chat_completion(request: ChatCompletionRequest,
 @with_cancellation
 @load_aware_call
 async def create_completion(request: CompletionRequest, raw_request: Request):
+
+    print("CREATE COMPLETION")
+
     handler = completion(raw_request)
     if handler is None:
         return base(raw_request).create_error_response(
