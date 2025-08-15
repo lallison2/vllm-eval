2b9528b0efbf624a91e8b25762c3e5c9512d01f0
lallison2 <lallison@mit.edu>
2025-08-15 16:18:41 -0700

    r

--- vllm/v1/engine/async_llm.py
+++ vllm/v1/engine/async_llm.py
@@ -233,9 +233,6 @@ class AsyncLLM(EngineClient):
         # Create a new output collector for the request.
         queue = RequestOutputCollector(output_kind=params.output_kind)
 
-        # if self.stat_loggers: # DELETE THIS
-        #     for stat_logger in self.stat_loggers[0]:
-        #         stat_logger.log_msg("PROCESSING REQUEST")
         print("PROCESSING REQUEST")
 
         # Convert Input --> Request.
@@ -244,10 +241,8 @@ class AsyncLLM(EngineClient):
             tokenization_kwargs, trace_headers, prompt_adapter_request,
             priority, data_parallel_rank,)
         
-        # if self.stat_loggers: # DELETE THIS SECTION
-        #     for stat_logger in self.stat_loggers[0]:
-        #         stat_logger.log_msg("inv_toks: ", str(inv_toks))
-        #         stat_logger.log_msg("detected?: ", str(detected_toks))
+        print("inv toks: ", inv_toks)
+        print("detected toks?", detected_toks)
 
         if is_pooling or params.n == 1:
             await self._add_request(request, prompt_str, None, 0, queue)
