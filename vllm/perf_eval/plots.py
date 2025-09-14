690dd85b367ab0f63c82d94c19fe24a010259e9e
lallison2 <lallison@mit.edu>
2025-09-14 17:42:57 -0400

    r

--- vllm/perf_eval/plots.py
+++ vllm/perf_eval/plots.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     ax.set_title("End-to-end Latency Comparison")
     ax.legend(fontsize=8, markerscale=0.7)
 
-    plt.savefig("plots/e2e_latency_prompt_len_gen-eval.png")
+    plt.savefig("plots/e2e_latency_prompt_len_eval.png")
 
     ###############################################
 
@@ -137,7 +137,7 @@ if __name__ == '__main__':
     # ax.set_title("Time-to-first-token Latency Comparison")
     # ax.legend(fontsize=8, markerscale=0.7)
 
-    # plt.savefig("plots/ttft_latency_prompt_len_gen-eval.png")
+    # plt.savefig("plots/ttft_latency_prompt_len_eval.png")
 
     ###############################################
 
@@ -197,7 +197,7 @@ if __name__ == '__main__':
     # ax.set_title("Request Queue Time Comparison")
     # ax.legend(fontsize=8, markerscale=0.7)
 
-    # plt.savefig("plots/queue_time_prompt_len_gen-eval.png")
+    # plt.savefig("plots/queue_time_prompt_len_eval.png")
 
     ###############################################
 
@@ -257,7 +257,7 @@ if __name__ == '__main__':
     # ax.set_title("Request Inference Time Comparison")
     # ax.legend(fontsize=8, markerscale=0.7)
 
-    # plt.savefig("plots/inference_time_prompt_len_gen-eval.png")
+    # plt.savefig("plots/inference_time_prompt_len_eval.png")
 
     ###############################################
 
@@ -317,64 +317,64 @@ if __name__ == '__main__':
     # ax.set_title("Request Prefill Time Comparison")
     # ax.legend(fontsize=8, markerscale=0.7)
 
-    # plt.savefig("plots/prefill_time_prompt_len_gen-eval.png")
+    # plt.savefig("plots/prefill_time_prompt_len_eval.png")
 
     ###############################################
 
-    target_metric = "vllm:request_decode_time_seconds_sum"
-    alora_metric_vals = extract_metrics_from_files(target_metric, is_alora=True)
-    lora_metric_vals = extract_metrics_from_files(target_metric, is_alora=False)
+    # target_metric = "vllm:request_decode_time_seconds_sum"
+    # alora_metric_vals = extract_metrics_from_files(target_metric, is_alora=True)
+    # lora_metric_vals = extract_metrics_from_files(target_metric, is_alora=False)
 
-    fig, ax = plt.subplots(figsize=(8, 6))
+    # fig, ax = plt.subplots(figsize=(8, 6))
 
-    from matplotlib.ticker import LogLocator, LogFormatterMathtext
-    ax.set_yscale('log')
-    ax.yaxis.set_major_formatter(LogFormatterMathtext(base=10))
-    ax.set_xscale('log')
-    ax.xaxis.set_major_locator(LogLocator(base=10.0))
-    ax.xaxis.set_major_formatter(LogFormatterMathtext(base=10.0))
+    # from matplotlib.ticker import LogLocator, LogFormatterMathtext
+    # ax.set_yscale('log')
+    # ax.yaxis.set_major_formatter(LogFormatterMathtext(base=10))
+    # ax.set_xscale('log')
+    # ax.xaxis.set_major_locator(LogLocator(base=10.0))
+    # ax.xaxis.set_major_formatter(LogFormatterMathtext(base=10.0))
 
-    ax.plot(prompt_lens, 
-            alora_metric_vals, 
-            label='ibm-granite/granite-3.2-8b-instruct (rank-32 aLoRA)', 
-            marker='D', 
-            markersize=4,
-            linestyle='-',
-            color="#6675A9",
-            markerfacecolor='#6675A9',
-            markeredgecolor='#6675A9',
-            )
-    ax.plot(prompt_lens, 
-            lora_metric_vals, 
-            label='ibm-granite/granite-3.2-8b-instruct (rank-8 LoRA)', 
-            marker='D', 
-            markersize=4,
-            linestyle='-',
-            color="#cdb38f",
-            markerfacecolor='#f4c5b5',
-            markeredgecolor='#f4c5b5',
-            )
+    # ax.plot(prompt_lens, 
+    #         alora_metric_vals, 
+    #         label='ibm-granite/granite-3.2-8b-instruct (rank-32 aLoRA)', 
+    #         marker='D', 
+    #         markersize=4,
+    #         linestyle='-',
+    #         color="#6675A9",
+    #         markerfacecolor='#6675A9',
+    #         markeredgecolor='#6675A9',
+    #         )
+    # ax.plot(prompt_lens, 
+    #         lora_metric_vals, 
+    #         label='ibm-granite/granite-3.2-8b-instruct (rank-8 LoRA)', 
+    #         marker='D', 
+    #         markersize=4,
+    #         linestyle='-',
+    #         color="#cdb38f",
+    #         markerfacecolor='#f4c5b5',
+    #         markeredgecolor='#f4c5b5',
+    #         )
     
-    ax.grid(
-        axis='x',
-        which='major',
-        linestyle='-',
-        linewidth=0.5,
-        color='gray',
-        alpha=0.7,
-    )
-    ax.grid(
-        axis='y',
-        which='major',
-        linestyle='-',
-        linewidth=0.5,
-        color='gray',
-        alpha=0.7,
-    )
+    # ax.grid(
+    #     axis='x',
+    #     which='major',
+    #     linestyle='-',
+    #     linewidth=0.5,
+    #     color='gray',
+    #     alpha=0.7,
+    # )
+    # ax.grid(
+    #     axis='y',
+    #     which='major',
+    #     linestyle='-',
+    #     linewidth=0.5,
+    #     color='gray',
+    #     alpha=0.7,
+    # )
 
-    ax.set_xlabel("Prompt Length")
-    ax.set_ylabel("Latency (s)")
-    ax.set_title("Request Decode Time Comparison")
-    ax.legend(fontsize=8, markerscale=0.7)
+    # ax.set_xlabel("Prompt Length")
+    # ax.set_ylabel("Latency (s)")
+    # ax.set_title("Request Decode Time Comparison")
+    # ax.legend(fontsize=8, markerscale=0.7)
 
-    plt.savefig("plots/decode_time_prompt_len_gen-eval.png")
+    # plt.savefig("plots/decode_time_prompt_len_eval.png")
