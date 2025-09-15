7753480382a53093bfb6ac6c7fc546685955496f
lallison2 <lallison@mit.edu>
2025-09-15 17:01:01 -0400

    r

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -157,7 +157,7 @@ async def main():
     random_prompts = []
     # current_prompt_len = prompt_lens[9] # max 9
     current_prompt_len = 256
-    current_gen_len = gen_lens[4] # max 9
+    current_gen_len = gen_lens[5] # max 9
     with open(f'prompts/random_prompt_len_{current_prompt_len}.txt', 'r') as f:
         for line in f:
             prompt_strings = line.strip().split(',')
