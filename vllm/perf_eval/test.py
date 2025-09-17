d0234f1f089e4e3079cb2c3476b1e90c26cdacaf
lallison2 <lallison@mit.edu>
2025-09-17 13:27:22 -0400

    r

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -266,11 +266,11 @@ async def main_poisson():
             Returns: Latency of evaluation task (manually timed using time.perf_counter())
             """
             # Call the base model
-            base_generation_tokens = await send(prompts, ntokens=current_gen_len, use_adapter_name=BASE_NAME)
+            base_generation_tokens = await send(prompts, ntokens=gen_len, use_adapter_name=BASE_NAME)
 
             # Call the adapter model
             adapter_prompts = [x + y + tokenizer("<|end_of_text|>\n")["input_ids"] for x,y in zip(prompts, base_generation_tokens)]
-            adapter_generation_tokens, eval_latency = await send(adapter_prompts, ntokens=16, use_adapter_name=ADAPTER_NAME, manually_time=True) 
+            adapter_generation_tokens, eval_latency = await send(adapter_prompts, ntokens=eval_len, use_adapter_name=ADAPTER_NAME, manually_time=True) 
 
             return eval_latency
         
