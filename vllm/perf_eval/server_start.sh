48a636bf70d0b064116eabbebe8014a0461e5298
lallison2 <lallison@mit.edu>
2025-09-17 14:26:01 -0400

    r

--- vllm/perf_eval/server_start.sh
+++ vllm/perf_eval/server_start.sh
@@ -8,24 +8,24 @@ export VLLM_USE_V1="1"
 # Note: Can load in multiple intrinsics here for easier testing later on.
 
 #### Start server with random alora ####
-vllm serve ibm-granite/granite-3.2-8b-instruct \
-    --enable-lora \
-    --dtype bfloat16 \
-    --max-lora-rank 64 \
-    --enable-prefix-caching \
-    --enable-activated-lora \
-    --lora-modules '{"name": "random_alora", "path": "/nobackup/users/lallison/hf_cache/hub/random_alora_r_32", "base_model_name": "ibm-granite/granite-3.2-8b-instruct"}' 
-    # --gpu-memory-utilization 0.7
-
-#### Start server with random lora ####
 # vllm serve ibm-granite/granite-3.2-8b-instruct \
 #     --enable-lora \
 #     --dtype bfloat16 \
 #     --max-lora-rank 64 \
 #     --enable-prefix-caching \
 #     --enable-activated-lora \
-#     --lora-modules '{"name": "random_lora", "path": "/nobackup/users/lallison/hf_cache/hub/random_lora_r_8", "base_model_name": "ibm-granite/granite-3.2-8b-instruct"}' 
+#     --lora-modules '{"name": "random_alora", "path": "/nobackup/users/lallison/hf_cache/hub/random_alora_r_32", "base_model_name": "ibm-granite/granite-3.2-8b-instruct"}' 
 #     # --gpu-memory-utilization 0.7
+
+#### Start server with random lora ####
+vllm serve ibm-granite/granite-3.2-8b-instruct \
+    --enable-lora \
+    --dtype bfloat16 \
+    --max-lora-rank 64 \
+    --enable-prefix-caching \
+    --enable-activated-lora \
+    --lora-modules '{"name": "random_lora", "path": "/nobackup/users/lallison/hf_cache/hub/random_lora_r_8", "base_model_name": "ibm-granite/granite-3.2-8b-instruct"}' 
+    # --gpu-memory-utilization 0.7
 
 # Check that the lora model is listed along with other models.
 #curl localhost:8000/v1/models | jq .
