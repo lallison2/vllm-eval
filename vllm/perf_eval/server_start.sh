7f122cbbf7adef894ff18d77701aea3429dd4260
lallison2 <lallison@mit.edu>
2025-09-25 12:36:10 -0400

    r

--- vllm/perf_eval/server_start.sh
+++ vllm/perf_eval/server_start.sh
@@ -11,6 +11,7 @@ export VLLM_LOG_LEVEL=debug
 #### Start server with random alora, granite-3.2-8b ####
 # vllm serve ibm-granite/granite-3.2-8b-instruct \
 #     --enable-lora \
+#     --port 8000 \
 #     --dtype bfloat16 \
 #     --max-lora-rank 64 \
 #     --enable-prefix-caching \
@@ -20,6 +21,7 @@ export VLLM_LOG_LEVEL=debug
 #### Start server with random lora, granite-3.2-8b ####
 # vllm serve ibm-granite/granite-3.2-8b-instruct \
 #     --enable-lora \
+    # --port 8000 \
 #     --dtype bfloat16 \
 #     --max-lora-rank 64 \
 #     --enable-prefix-caching \
@@ -29,6 +31,7 @@ export VLLM_LOG_LEVEL=debug
 # ### Start server with random alora, meta-llama/Llama-3.3-70B-Instruct ####
 vllm serve /nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b \
     --tensor-parallel-size 4 \
+    --port 8000 \
     --enable-lora \
     --dtype bfloat16 \
     --max-lora-rank 64 \
@@ -39,6 +42,7 @@ vllm serve /nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-I
 ### Start server with random lora, meta-llama/Llama-3.3-70B-Instruct ####
 # vllm serve /nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b \
 #     --tensor-parallel-size 4 \
+#     --port 8000 \
 #     --enable-lora \
 #     --dtype bfloat16 \
 #     --max-lora-rank 64 \
