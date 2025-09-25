590dad5cfc94de32cb1fe2d704b49289645a68ad
lallison2 <lallison@mit.edu>
2025-09-25 12:01:25 -0400

    r

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -214,7 +214,7 @@ async def main():
     # Call the adapter model
     adapter_prompts = [x + y + tokenizer("<|end_of_text|>\n")["input_ids"] for x,y in zip(random_prompts, base_generation_tokens)]
 
-    print(f"final length: {len(adapter_prompts[0] + 16)}")
+    print(f"final length: {len(adapter_prompts[0]) + 16}")
 
     adapter_start_stat_vals, adapter_start_hist_vals = await get_metrics(stats, histograms)
     adapter_generation_tokens = await send(adapter_prompts, ntokens=num_eval_tokens, use_adapter_name=ADAPTER_NAME) 
