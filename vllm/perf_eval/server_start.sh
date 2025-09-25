749a8161f115cc8497bb033b3a75ab1714eb6438
lallison2 <lallison@mit.edu>
2025-09-24 21:02:25 -0400

    r

--- vllm/perf_eval/server_start.sh
+++ vllm/perf_eval/server_start.sh
@@ -28,13 +28,13 @@ export VLLM_USE_V1="1"
 #     # --gpu-memory-utilization 0.7
 
 #### Start server with random alora, meta-llama/Llama-3.3-70B-Instruct ####
-vllm serve /nobackup/users/lallison/hf_cache/models--?/snapshots/? \
+vllm serve /nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b \
     --enable-lora \
     --dtype bfloat16 \
     --max-lora-rank 64 \
     --enable-prefix-caching \
     --enable-activated-lora \
-    --lora-modules '{"name": "random_alora", "path": "/nobackup/users/lallison/hf_cache/hub/random_alora_r_32", "base_model_name": "?"}' 
+    --lora-modules '{"name": "random_alora", "path": "/nobackup/users/lallison/hf_cache/hub/random_alora_r_32", "base_model_name": "meta-llama/Llama-3.3-70B-Instruct"}' 
     # --gpu-memory-utilization 0.7
 
 # Check that the lora model is listed along with other models.
