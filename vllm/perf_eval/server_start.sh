261ef5aa0e5f36c0a2c4aa1ffa8a2c1970509d56
lallison2 <lallison@mit.edu>
2025-09-24 22:05:35 -0400

    r

--- vllm/perf_eval/server_start.sh
+++ vllm/perf_eval/server_start.sh
@@ -27,7 +27,18 @@ export VLLM_USE_V1="1"
 #     --lora-modules '{"name": "random_lora", "path": "/nobackup/users/lallison/hf_cache/hub/random_lora_r_8", "base_model_name": "ibm-granite/granite-3.2-8b-instruct"}' 
 #     # --gpu-memory-utilization 0.7
 
-#### Start server with random alora, meta-llama/Llama-3.3-70B-Instruct ####
+# ### Start server with random alora, meta-llama/Llama-3.3-70B-Instruct ####
+# vllm serve /nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b \
+#     --tensor-parallel-size 4 \
+#     --enable-lora \
+#     --dtype bfloat16 \
+#     --max-lora-rank 64 \
+#     --enable-prefix-caching \
+#     --enable-activated-lora \
+#     --lora-modules '{"name": "random_alora", "path": "/nobackup/users/lallison/hf_cache/hub/random_alora_r_32", "base_model_name": "meta-llama/Llama-3.3-70B-Instruct"}' 
+#     # --gpu-memory-utilization 0.7
+
+### Start server with random lora, meta-llama/Llama-3.3-70B-Instruct ####
 vllm serve /nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b \
     --tensor-parallel-size 4 \
     --enable-lora \
@@ -35,7 +46,7 @@ vllm serve /nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-I
     --max-lora-rank 64 \
     --enable-prefix-caching \
     --enable-activated-lora \
-    --lora-modules '{"name": "random_alora", "path": "/nobackup/users/lallison/hf_cache/hub/random_alora_r_32", "base_model_name": "meta-llama/Llama-3.3-70B-Instruct"}' 
+    --lora-modules '{"name": "random_lora", "path": "/nobackup/users/lallison/hf_cache/hub/random_lora_r_8", "base_model_name": "meta-llama/Llama-3.3-70B-Instruct"}' 
     # --gpu-memory-utilization 0.7
 
 # Check that the lora model is listed along with other models.
