c7b76f3e6c39e5446dc7c5255e4fb02c2e3cb60c
lallison2 <lallison@mit.edu>
2025-09-24 21:07:44 -0400

    r

--- vllm/perf_eval/server_start.sh
+++ vllm/perf_eval/server_start.sh
@@ -29,6 +29,7 @@ export VLLM_USE_V1="1"
 
 #### Start server with random alora, meta-llama/Llama-3.3-70B-Instruct ####
 vllm serve /nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b \
+    --tensor-parallel-size 4 \
     --enable-lora \
     --dtype bfloat16 \
     --max-lora-rank 64 \
