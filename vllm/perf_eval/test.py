86a0982106fd57f39eb94ea3bcf8be72a2666d85
lallison2 <lallison@mit.edu>
2025-09-29 14:52:04 -0400

    r

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -173,7 +173,7 @@ async def main():
                 ]
     
     random_prompts = []
-    current_prompt_len = prompt_lens[8] # max 9
+    current_prompt_len = prompt_lens[9] # max 9
     # current_prompt_len = 256
     # current_gen_len = gen_lens[7] # max 7
     current_gen_len = 256
