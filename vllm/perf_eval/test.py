1fb7f5390fe9c2ddfdfa2f48d7b19897103a8b13
lallison2 <lallison@mit.edu>
2025-09-30 18:10:54 -0400

    r

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -175,7 +175,7 @@ async def main():
     random_prompts = []
     # current_prompt_len = prompt_lens[9] # max 9
     current_prompt_len = 256
-    current_gen_len = gen_lens[4] # max 7
+    current_gen_len = gen_lens[5] # max 7
     # current_gen_len = 256
     with open(f'prompts/random_prompt_len_{current_prompt_len}.txt', 'r') as f:
         for line in f:
