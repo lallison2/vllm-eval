0d07307c29c53d106c79b3bd5aae6ff46a44d129
lallison2 <lallison@mit.edu>
2025-10-06 16:27:07 -0400

    r

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -14,8 +14,8 @@ import random
 import math
 
 # BASE_NAME = "/nobackup/users/lallison/hf_cache/models--ibm-granite--granite-3.2-8b-instruct/snapshots/610d8c6ee9c84ce51f6dfd7bc5c0215d95d49695"
-BASE_NAME = "/nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b"
-# BASE_NAME = "/nobackup/users/lallison/hf_cache/models--mistralai--Mistral-Large-Instruct-2407/snapshots/a286006d554cb37a61d13c7ae61bc90cc1d372fc"
+# BASE_NAME = "/nobackup/users/lallison/hf_cache/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b"
+BASE_NAME = "/nobackup/users/lallison/hf_cache/models--mistralai--Mistral-Large-Instruct-2407/snapshots/a286006d554cb37a61d13c7ae61bc90cc1d372fc"
 
 ALORA_NAME = "random_alora"
 LORA_NAME = "random_lora"
