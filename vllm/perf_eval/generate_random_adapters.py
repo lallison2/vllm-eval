2b6760089ac5c78ab66c33cb243a4eec791258d7
lallison2 <lallison@mit.edu>
2025-10-13 16:23:55 -0400

    s

--- vllm/perf_eval/generate_random_adapters.py
+++ vllm/perf_eval/generate_random_adapters.py
@@ -9,7 +9,7 @@ class ALoraConfig(LoraConfig):
 
 torch.manual_seed(40)
 base_model_name = "/nobackup/users/lallison/hf_cache/models--ibm-granite--granite-3.2-8b-instruct/snapshots/610d8c6ee9c84ce51f6dfd7bc5c0215d95d49695"
-alora_save_path = "/nobackup/users/lallison/hf_cache/hub/random_alora_r_32_5"
+alora_save_path = "/nobackup/users/lallison/hf_cache/hub/random_alora_r_32_2"
 
 base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.float16)
 alora_config = ALoraConfig(
@@ -25,7 +25,7 @@ alora_model = get_peft_model(base_model, alora_config)
 
 for name, param in alora_model.named_parameters():
     if "lora_A" in name or "lora_B" in name:
-        param.data = torch.randn_like(param.data) * 0.2
+        param.data = torch.randn_like(param.data) * 0.01
 print("random alora weights initialized (rank 32)")
 
 alora_model.save_pretrained(alora_save_path)
@@ -33,7 +33,7 @@ alora_model.save_pretrained(alora_save_path)
 #####################################################################
 
 alora_model.unload()
-lora_save_path = "/nobackup/users/lallison/hf_cache/hub/random_lora_r_8_5"
+lora_save_path = "/nobackup/users/lallison/hf_cache/hub/random_lora_r_8_2"
 
 base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.float16)
 lora_config = LoraConfig(
@@ -48,7 +48,7 @@ lora_model = get_peft_model(base_model, lora_config)
 
 for name, param in lora_model.named_parameters():
     if "lora_A" in name or "lora_B" in name:
-        param.data = torch.randn_like(param.data) * 0.2
+        param.data = torch.randn_like(param.data) * 0.01
 print("random lora weights initialized (rank 8)")
 
 lora_model.save_pretrained(lora_save_path)
