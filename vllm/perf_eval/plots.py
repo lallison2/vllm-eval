59859ab46e9f352f7dc253598c5aca5c99522fdb
lallison2 <lallison@mit.edu>
2025-10-14 17:11:59 -0400

    s

--- vllm/perf_eval/plots.py
+++ vllm/perf_eval/plots.py
@@ -12,7 +12,7 @@ component = "eval"
 
 component_title = {'eval': 'Evaluation', 'gen_1': 'First Generation', 'gen_2': 'Second Generation'}
 
-def extract_metrics_from_files(target_metric, component="prompt_len", is_alora=False, path_prefix="", path_suffix="", component=component, varying_comp="gen_len"):
+def extract_metrics_from_files(target_metric, is_alora=False, path_prefix="", path_suffix="", component=component, varying_comp="gen_len"):
     metric_vals = []
 
     varying_list = gen_lens
