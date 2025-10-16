26562806c86d2961beedab58dd8b23b317223d1c
lallison2 <lallison@mit.edu>
2025-10-16 19:40:58 -0400

    s

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -350,23 +350,23 @@ async def main_poisson():
             # Call the base model
             base_generation_tokens = await send(prompts, ntokens=gen_len + num_eot_tokens, use_adapter_name=BASE_NAME)
 
-            # # Call the adapter model
-            # adapter_prompts = [x + y[:gen_len] + tokenizer("<|end_of_text|>\n")["input_ids"] for x,y in zip(prompts, base_generation_tokens)]
-            # adapter_generation_tokens, eval_latency = await send(adapter_prompts, ntokens=eval_len, use_adapter_name=ADAPTER_NAME, manually_time=True) 
+            # Call the adapter model
+            adapter_prompts = [x + y[:gen_len] + tokenizer("<|end_of_text|>\n")["input_ids"] for x,y in zip(prompts, base_generation_tokens)]
+            adapter_generation_tokens, eval_latency = await send(adapter_prompts, ntokens=eval_len, use_adapter_name=ADAPTER_NAME, manually_time=True) 
 
-            # return eval_latency
+            return eval_latency
         
         task = asyncio.create_task(gen_eval_send(prompts=[random_prompts[i]], gen_len=current_gen_len, eval_len=16))
         tasks.append(task)
 
-    # eval_latencies = await asyncio.gather(*tasks) # wait until all requests have finished
+    eval_latencies = await asyncio.gather(*tasks) # wait until all requests have finished
 
-    # # Get current Prometheus metrics
-    # adapter_stat_vals, adapter_hist_vals = await get_metrics(stats, histograms)
+    # Get current Prometheus metrics
+    adapter_stat_vals, adapter_hist_vals = await get_metrics(stats, histograms)
     
-    # # Subtract the metrics from the warmup call
-    # final_stat_vals, final_hist_vals = subtract_metrics(adapter_stat_vals, adapter_hist_vals, earlier_stat_vals, earlier_hist_vals)
-    # save_metrics(final_stat_vals, final_hist_vals, ADAPTER_NAME, file_name=f"results/lora_async_poisson_{LAMBDA}rps_prompt_len_1024.txt", manually_timed_eval_latencies=eval_latencies)
+    # Subtract the metrics from the warmup call
+    final_stat_vals, final_hist_vals = subtract_metrics(adapter_stat_vals, adapter_hist_vals, earlier_stat_vals, earlier_hist_vals)
+    save_metrics(final_stat_vals, final_hist_vals, ADAPTER_NAME, file_name=f"results/lora_async_poisson_{LAMBDA}rps_prompt_len_1024.txt", manually_timed_eval_latencies=eval_latencies)
 
 
 ###################################################################
