030df3b8bb2ae1acc9bbb321a6ea1d39d0ccfc91
lallison2 <lallison@mit.edu>
2025-10-16 00:09:08 -0400

    s

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -227,10 +227,10 @@ async def main():
 
     # Call the base model
     # base_start_stat_vals, base_start_hist_vals = await get_metrics(stats, histograms)
-    base_generation_tokens = await send(random_prompts, ntokens=current_gen_len, use_adapter_name=BASE_NAME)
+    base_generation_tokens = await send(random_prompts, ntokens=current_gen_len + 2, use_adapter_name=BASE_NAME)
 
     # Call the adapter model(s)
-    adapter_prompts = [x + y + tokenizer("<|end_of_text|>\n")["input_ids"] for x,y in zip(random_prompts, base_generation_tokens)]
+    adapter_prompts = [x + y[:current_gen_len] + tokenizer("<|end_of_text|>\n")["input_ids"] for x,y in zip(random_prompts, base_generation_tokens)]
     print("random prompt len: ", len(random_prompts[0]))
     print("response len: ", len(base_generation_tokens[0]))
     print("eot len: ", len(tokenizer("<|end_of_text|>\n")["input_ids"]))
