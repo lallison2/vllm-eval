5206a3103264028bd727c0317c5ab1a0904f1cb7
lallison2 <lallison@mit.edu>
2025-10-16 23:31:30 -0400

    s

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -310,7 +310,7 @@ async def main_poisson():
             random_prompts.append(prompt_tokens)
     
     lambdas = [0.5, 1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 20000, 50000] # requests per second
-    LAMBDA = lambdas[5] # max 11
+    LAMBDA = lambdas[4] # max 11
     TOTAL_REQUESTS = 500 # reasonably large value
     random.seed(15)
     np.random.seed(100)
