69550bd0338987aed2f39d48008a72dd03be47b7
lallison2 <lallison@mit.edu>
2025-10-17 22:51:43 -0400

    s

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -182,7 +182,7 @@ async def main():
     # current_prompt_len = prompt_lens[3] # max 9
     # current_gen_len = 256
 
-    current_gen_len = gen_lens[3] # max 7
+    current_gen_len = gen_lens[5] # max 7
     current_prompt_len = 256
 
     with open(f'prompts/random_prompt_len_{current_prompt_len}.txt', 'r') as f:
@@ -230,7 +230,7 @@ async def main():
 
     # Call the base model
     # base_start_stat_vals, base_start_hist_vals = await get_metrics(stats, histograms)
-    base_generation_tokens = await send(random_prompts, ntokens=current_gen_len, use_adapter_name=BASE_NAME) # generate extra tokens to ensure that model doesn't stop early
+    base_generation_tokens = await send(random_prompts, ntokens=current_gen_len + num_eot_tokens, use_adapter_name=BASE_NAME) # generate extra tokens to ensure that model doesn't stop early
 
     # Call the adapter model(s)
     adapter_prompts = [x + y[:current_gen_len] + tokenizer("<|end_of_text|>\n")["input_ids"] for x,y in zip(random_prompts, base_generation_tokens)]
