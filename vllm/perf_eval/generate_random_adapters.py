b60bf899bcbf23ee5abc2b676bb3ff7f99803d88
lallison2 <lallison@mit.edu>
2025-10-19 15:13:39 -0400

    s

--- vllm/perf_eval/generate_random_adapters.py
+++ vllm/perf_eval/generate_random_adapters.py
@@ -7,9 +7,9 @@ class ALoraConfig(LoraConfig):
         super().__init__(**kwargs)
         self.invocation_string = invocation_string
 
-torch.manual_seed(70)
+torch.manual_seed(80)
 base_model_name = "/home/lallison/.cache/huggingface/hub/models--ibm-granite--granite-3.2-8b-instruct/snapshots/610d8c6ee9c84ce51f6dfd7bc5c0215d95d49695"
-alora_save_path = "/home/lallison/vllm-eval/vllm/perf_eval/random_alora_r_32_granite_4"
+alora_save_path = "/home/lallison/vllm-eval/vllm/perf_eval/random_alora_r_32_granite_5"
 
 base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.float16)
 alora_config = ALoraConfig(
@@ -33,7 +33,7 @@ alora_model.save_pretrained(alora_save_path)
 #####################################################################
 
 alora_model.unload()
-lora_save_path = "/home/lallison/vllm-eval/vllm/perf_eval/random_lora_r_8_granite_4"
+lora_save_path = "/home/lallison/vllm-eval/vllm/perf_eval/random_lora_r_8_granite_5"
 
 base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.float16)
 lora_config = LoraConfig(
