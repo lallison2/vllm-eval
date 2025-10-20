dcf3c79a632bce72d8603f4937c8cd743ea15590
lallison2 <lallison@mit.edu>
2025-10-20 16:40:26 -0400

    s

--- vllm/perf_eval/test.py
+++ vllm/perf_eval/test.py
@@ -183,7 +183,7 @@ async def main():
     # current_prompt_len = prompt_lens[3] # max 9
     # current_gen_len = 256
 
-    current_gen_len = gen_lens[4] # max 8 (didn't do 8 for alora yet)
+    current_gen_len = gen_lens[5] # max 8 (didn't do 8 for alora yet)
     current_prompt_len = 256
 
     with open(f'prompts/random_prompt_len_{current_prompt_len}.txt', 'r') as f:
