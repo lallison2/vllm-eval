db24c0f939b47fa634132bfbf68380991c6a5106
lallison2 <lallison@mit.edu>
2025-10-20 17:59:06 -0400

    s

--- vllm/perf_eval/plots.py
+++ vllm/perf_eval/plots.py
@@ -2176,6 +2176,7 @@ if __name__ == '__main__':
     ax.set_xscale('log')
     ax.xaxis.set_major_locator(LogLocator(base=10.0))
     ax.xaxis.set_major_formatter(LogFormatterMathtext(base=10.0))
+    ax.set_ylim(1.0, 1.2)
 
     ax.plot(gen_lens, 
             [a / b for a, b in zip(granite_lora_metric_vals, granite_alora_metric_vals)], 
