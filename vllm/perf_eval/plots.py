832d18ce493563a1039a3846acce5183869cc8c5
lallison2 <lallison@mit.edu>
2025-10-22 17:51:11 -0400

    s

--- vllm/perf_eval/plots.py
+++ vllm/perf_eval/plots.py
@@ -2651,8 +2651,8 @@ if __name__ == '__main__':
     ax.set_title('Throughput Comparison (Prompt Length = 65536)', fontsize=20)
     ax.set_xticks(x + width, models)
     ax.tick_params(axis='both', which='major', labelsize=18)
-    ax.legend(fontsize=16, markerscale=1.0, bbox_to_anchor=(0.5, -0.15))
-
+    ax.legend(fontsize=16, markerscale=1.0, bbox_to_anchor=(0.5, -0.35))
+    plt.tight_layout(rect=[0, 0.1, 1, 1])
 
     # plt.tight_layout()
 
