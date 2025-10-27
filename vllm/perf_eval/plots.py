f9ce72c7ee554c1bb66459e6fe43bb15d6d2432a
lallison2 <lallison@mit.edu>
2025-10-27 00:41:10 -0400

    s

--- vllm/perf_eval/plots.py
+++ vllm/perf_eval/plots.py
@@ -763,10 +763,9 @@ if __name__ == '__main__':
     plt.savefig(f"plots/base_adapter_decode_time_prompt_len-{component}.png")
 
     # Create a separate legend
-    fig2, ax2 = plt.subplots()
+    fig2, ax2 = plt.subplots(constrained_layout=True)
     ax2.legend(handles, labels, loc='center', fontsize=20, markerscale=1.0, ncol=3)
     ax2.axis('off')  # Hide axes
-    plt.tight_layout()
     plt.savefig(f"plots/base_adapter_prompt_len-legend.png")
 
     ###############################################
@@ -1267,10 +1266,9 @@ if __name__ == '__main__':
     plt.savefig(f"plots/base_adapter_decode_time_speedup_factor_prompt_len-{component}.png")
 
     # Create a separate legend
-    fig2, ax2 = plt.subplots()
+    fig2, ax2 = plt.subplots(constrained_layout=True)
     ax2.legend(handles, labels, loc='center', fontsize=20, markerscale=1.0, ncol=3)
     ax2.axis('off')  # Hide axes
-    plt.tight_layout()
     plt.savefig(f"plots/base_adapter_prompt_len_speedup-legend.png")
 
     ##############################################
